097906256eac67c070a5d5a170bf56edaa700bf7
timumuti <timuemuti@gmail.com>
2025-08-27 20:40:20 -0300

    Add files via upload

---
+++ ProjetodoSemestre/projetoescola/src/main/java/com/example/projetoescola/ProjetoescolaApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetoescola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoescolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoescolaApplication.class, args);
+	}
+
+}
