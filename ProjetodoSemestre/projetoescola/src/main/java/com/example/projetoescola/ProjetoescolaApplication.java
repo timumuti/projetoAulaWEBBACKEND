9c67ab60db4706e22c50af9bd6b05ce38f8f062a
timumuti <timuemuti@gmail.com>
2025-08-27 22:17:37 -0300

    Add files via upload

--- ProjetodoSemestre/projetoescola/src/main/java/com/example/projetoescola/ProjetoescolaApplication.java
+++ ProjetodoSemestre/projetoescola/src/main/java/com/example/projetoescola/ProjetoescolaApplication.java
@@ -1,12 +1,32 @@
 package com.example.projetoescola;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.projetoescola.models.Curso;
+import com.example.projetoescola.repositors.CursoRepositer;
 
 @SpringBootApplication
 public class ProjetoescolaApplication {
 
-	public static void main(String[] args) {
+	@Bean
+public CommandLineRunner init(@Autowired CursoRepositer cursoRepository) {
+    return args -> {
+     cursoRepository.salvar(
+    new Curso(null, "teste",2000));
+     cursoRepository.salvar(
+    new Curso(null, "teste2", 2050));
+List<Curso> listaCursos = cursoRepository.obterTodos();
+listaCursos.forEach(System.out::println);
+};
+} 
+
+		public static void main(String[] args) {
 		SpringApplication.run(ProjetoescolaApplication.class, args);
 	}
 
---
+++ ProjetodoSemestre/projetoescola/src/main/java/com/example/projetoescola/models/Curso.java
@@ -0,0 +1,53 @@
+package com.example.projetoescola.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity  // vai transformar numa tabela para banco de dados
+public class Curso {
+    @Id  //primaryKey 
+    @GeneratedValue (strategy = GenerationType.IDENTITY)  // vai transforma minha chave primaria em auto incrementativo
+    private Long id;
+    @Column(length = 200, nullable = false)  // para limitar a 200 caracteres o nome e tambem não aceitar nulos
+    private String nome;
+    @Column(nullable = false)      // carga horaria não pode ser nula, @COLUMN não é obrigatorio, mas se quiser colocar --
+    private Integer cargaHoraria;   //consições nas colunas, precisa por
+   
+
+    public Curso(Long id, String nome, Integer cargaHoraria) { // com os parametros
+        this.id = id;
+        this.nome = nome;
+        this.cargaHoraria = cargaHoraria;
+    }
+    public Curso() {   // vazio
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Integer getCargaHoraria() {
+        return cargaHoraria;
+    }
+    public void setCargaHoraria(Integer cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+
+    @Override     // esse é o "toString" é o q vai mudar a forma que vai ser imressa a informação
+    public String toString() {
+        return "Curso [id=" + id + ", nome=" + nome + ", cargaHoraria=" + cargaHoraria + "]";
+    }
+    
+}
